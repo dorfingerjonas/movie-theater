1fb7232bd5953c0da84b970cb09dc3877e8a1973
Jonas Dorfinger <contact@dorfingerjonas.at>
2021-11-21 11:57:48 +0100

    implement findByTitle() method

--- src/main/java/at/htl/movietheater/control/MovieRepository.java
+++ src/main/java/at/htl/movietheater/control/MovieRepository.java
@@ -5,6 +5,8 @@ import at.htl.movietheater.entity.Movie;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
@@ -16,5 +18,16 @@ public class MovieRepository {
     @Transactional
     public Movie save(Movie movie) {
         return em.merge(movie);
+    }
+
+    public Movie findByTitle(String title) {
+        try {
+            TypedQuery<Movie> query = em.createNamedQuery("Movie.findByTitle", Movie.class)
+                .setParameter("title", title);
+
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 }
