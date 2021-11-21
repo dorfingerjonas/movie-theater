deab0e4dcde4b8de88be5912f4e0e88b2d082644
Jonas Dorfinger <contact@dorfingerjonas.at>
2021-11-21 12:18:47 +0100

    implement ShowRepository

---
+++ src/main/java/at/htl/movietheater/control/ShowRepository.java
@@ -0,0 +1,44 @@
+package at.htl.movietheater.control;
+
+import at.htl.movietheater.entity.Show;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class ShowRepository {
+
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public Show save(Show show) {
+        show = em.merge(show);
+
+        if (show.getPrevShow() == null) {
+            show.setPrevShow(findLastShow());
+        }
+
+        if (show.getPrevShow() != null) {
+            show.getPrevShow().setNextShow(show);
+        }
+
+        return show;
+    }
+
+    public Show findLastShow() {
+        try {
+            TypedQuery<Show> query = em
+                .createNamedQuery("Show.findLastShow", Show.class);
+
+            return query.setMaxResults(1).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+}
