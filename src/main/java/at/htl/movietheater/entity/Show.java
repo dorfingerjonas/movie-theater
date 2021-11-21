88e47b768bf084f088ad867e9d79791bbd8d2f5c
Jonas Dorfinger <contact@dorfingerjonas.at>
2021-11-21 09:11:30 +0100

    implement entity Show

---
+++ src/main/java/at/htl/movietheater/entity/Show.java
@@ -0,0 +1,87 @@
+package at.htl.movietheater.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "MT_SHOW")
+@SequenceGenerator(
+        name = "showSeq",
+        initialValue = 1000
+)
+public class Show {
+
+    @Id
+    @Column(name = "SH_ID")
+    @GeneratedValue(generator = "showSeq")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "SH_MO_ID")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "SH_TH_ID")
+    private Theater theater;
+
+    @ManyToOne
+    @JoinColumn(name = "SH_PREV_SHOW_ID")
+    private Show prevShow;
+
+    @ManyToOne
+    @JoinColumn(name = "SH_NEXT_SHOW_ID")
+    private Show nextShow;
+
+    public Show() {
+    }
+
+    public Show(Movie movie, Theater theater, Show prevShow, Show nextShow) {
+        this.movie = movie;
+        this.theater = theater;
+        this.prevShow = prevShow;
+        this.nextShow = nextShow;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Theater getTheater() {
+        return theater;
+    }
+
+    public void setTheater(Theater theater) {
+        this.theater = theater;
+    }
+
+    public Show getPrevShow() {
+        return prevShow;
+    }
+
+    public void setPrevShow(Show prevShow) {
+        this.prevShow = prevShow;
+    }
+
+    public Show getNextShow() {
+        return nextShow;
+    }
+
+    public void setNextShow(Show nextShow) {
+        this.nextShow = nextShow;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s shown in %s", movie.getTitle(), theater.getName());
+    }
+}
